f637b02a92abb73908474e6bb265a77990918eda
charles-winit <2323849131@qq.com>
2024-04-12 16:30:10 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,19 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(applenum: i32)->i32 {
+    if applenum<=40
+    {
+        applenum*2
+    }
+    else 
+    {
+        applenum
+    }
 
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
